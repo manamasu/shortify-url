ce001c6686eb2dbacab6502e2bd07d0f66b90310
manamasu <97730788+manamasu@users.noreply.github.com>
2025-03-12 06:28:47 +0100

    refactor: display urls in a card format, wip

--- frontend/src/components/URLCard.jsx
+++ frontend/src/components/URLCard.jsx
@@ -1,8 +1,10 @@
-export default function URLCard() {
+export default function URLCard({ title, short_url }) {
   return (
-    <div className="flex flex-col">
-      <h2>Title</h2>
-      <p>Example URL</p>
+    <div className="flex flex-wrap m-2 pb-2 border-2 rounded w-68">
+      <div className="p-2 flex-1">
+        <h2>{title}</h2>
+        <p>{short_url}</p>
+      </div>
     </div>
   );
 }
--- frontend/src/components/URLList.jsx
+++ frontend/src/components/URLList.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import URLCard from "./URLCard";
+
 export default function URLList() {
+  const [urls, setURLs] = useState([]);
+
+  useEffect(() => {
+    const fetchURLs = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/v1/urls");
+        console.log(response.data);
+
+        setURLs(response.data);
+      } catch (err) {
+        console.log("Theere was an error retrieving all urls", err);
+      }
+    };
+    fetchURLs();
+  }, []);
+
+  urls.map((url) => console.log(url));
+
   return (
     <div className="flex-1 border-2">
-      <h1>My URLs</h1>
+      <h1 className="m-2 font-semibold">My URLs</h1>
+      {urls.map((url) => {
+        return <URLCard title={url.title} short_url={url.short_url} />;
+      })}
     </div>
   );
 }
